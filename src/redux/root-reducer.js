f44f28c51ee82a44de48df77b7d628fba36fae37
mohamed eltantawy <m.eltantawy17@gmail.com>
2019-10-08 00:51:09 +0200

    added redux persist lib

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,24 @@
 import { combineReducers } from 'redux';
 
+///redux persist step 2
+import { persistReducer } from 'redux-persist';
+//local storage
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer'
-export default combineReducers({
+import cartReducer from './cart/cart.reducer';
+
+//persist config
+const persistConfig = {
+    key: 'root',
+    storage,
+    //white list is for the reducer we want to presist
+    whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
